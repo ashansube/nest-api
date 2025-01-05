28880baf3f5f47c6c6cd8e4816ade7930f303049
ashansube <ashan.sql@gmail.com>
2025-01-05 22:12:06 +0530

    pipes and guards

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -5,14 +5,18 @@ import {
   Get,
   NotFoundException,
   Param,
+  ParseIntPipe,
   Post,
   Put,
   Query,
+  UseGuards,
+  ValidationPipe,
 } from '@nestjs/common';
 import { query } from 'express';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
 import { NinjasService } from './ninjas.service';
+import { BeltGuard } from './belt/belt.guard';
 
 @Controller('ninjas')
 export class NinjasController {
@@ -25,26 +29,30 @@ export class NinjasController {
   }
 
   @Get(':id')
-  getOneNinja(@Param('id') id: string) {
-    try{
-        return this.ninjaService.getOneNinja(parseInt(id));
-    }catch (err) {
-        throw new NotFoundException();
+  getOneNinja(@Param('id', ParseIntPipe) id: number) {
+    try {
+      return this.ninjaService.getOneNinja(id);
+    } catch (err) {
+      throw new NotFoundException();
     }
   }
 
   @Post()
-  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+  @UseGuards(BeltGuard)
+  createNinja(@Body(new ValidationPipe()) createNinjaDto: CreateNinjaDto) {
     return this.ninjaService.createNinja(createNinjaDto);
   }
 
   @Put(':id')
-  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
-    return this.ninjaService.updateNinja(parseInt(id), updateNinjaDto);
+  updateNinja(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateNinjaDto: UpdateNinjaDto,
+  ) {
+    return this.ninjaService.updateNinja(id, updateNinjaDto);
   }
 
   @Delete(':id')
-  removeNinja(@Param('id') id: string) {
-    return this.ninjaService.removeNinja(parseInt(id));
+  removeNinja(@Param('id', ParseIntPipe) id: number) {
+    return this.ninjaService.removeNinja(id);
   }
 }
